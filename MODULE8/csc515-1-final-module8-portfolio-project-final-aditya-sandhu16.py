f1da908ba00de810943e5a62ede2c5b3cd59d9ab
Adi Sandhu <adi.sandhu@outlook.com>
2025-11-01 12:15:38 -0600

    udpates

---
+++ MODULE8/csc515-1-final-module8-portfolio-project-final-aditya-sandhu16.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""
+Face-Anonymizer – Module 8 Portfolio Project
+Author: Aditya Sandhu
+Description: This script processes images to detect human faces using pre-trained classifiers
+"""
